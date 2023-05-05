56e28975c3d40d25f8de649db827635a049de5fd
Cleyton Gomes <cleytongomesx@gmail.com>
2023-05-05 17:06:11 -0300

    Primeiro teste da nova feature de interface web

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import configparser
-import csv
 import os
 import shutil
+from shutil import make_archive
+
 import tqdm
 from PIL import Image
+from pywebio import config, input, output, start_server
 
 # leitura do arquivo de configuração
 configparser = configparser.ConfigParser()
@@ -17,29 +19,56 @@ redimensionar = configparser.getboolean('config', 'redimensionar')
 altura = configparser.getint('resize', 'altura')
 largura = configparser.getint('resize', 'largura')
 
-# abre arquivo de produtos
-file = open(arquivo, 'r')
-reader = csv.reader(file)
+@config(theme="dark")
+def main():
 
-# percorre arquivo de produtos
-for linha in tqdm.tqdm(reader, desc='Copiando imagens', unit=' linhas'):
+    output.put_markdown('## DOWNLOAD DE PRODUTOS 3F')
 
-    # Ajusta o produto
-    produto = linha[0].strip().upper()
-    produto = produto.replace('/', '_')
+    # Lista de produtos ajustada
+    produtos = input.textarea("Nome dos produtos a serem baixados", rows=8, placeholder="Produtos que deseja realizar o download")
+    lista_produtos = list(map(lambda x: x.strip(),produtos.split('\n'))) # transforma em lista e remove espaços
+    lista_produtos = list(filter(bool, lista_produtos)) # remove elementos vazios
 
-    # verifica se o produto existe como imagem na pasta origem
-    if (produto + '.jpg') in os.listdir(pasta_origem):
+    # Remove pasta de destino e cria novamente para apagar os arquivos
+    shutil.rmtree(pasta_destino, ignore_errors=True)
+    os.mkdir(pasta_destino)
 
-        if redimensionar:
-            # redimensiona imagem e move para pasta destino
-            img = Image.open(os.path.join(pasta_origem, produto + '.jpg'))
-            img_resized = img.resize((largura, altura))
-            img_resized.save(os.path.join(pasta_destino, produto + '.jpg'))
+    # Percorre os produtos listados
+    for produto in tqdm.tqdm(lista_produtos, desc='Copiando imagens', unit=' Produtos'):
+
+        # Ajusta o produto
+        produto = produto.upper().replace('/', '_')
+
+        # verifica se o produto existe como imagem na pasta origem
+        if (produto + '.jpg') in os.listdir(pasta_origem):
+
+            if redimensionar:
+                # redimensiona imagem e move para pasta destino
+                img = Image.open(os.path.join(pasta_origem, produto + '.jpg'))
+                img_resized = img.resize((largura, altura))
+                img_resized.save(os.path.join(pasta_destino, produto + '.jpg'))
+            else:
+                # copia imagem para pasta destino
+                shutil.copy(os.path.join(pasta_origem, produto + '.jpg'), pasta_destino)
 
         else:
-            # copia imagem para pasta destino
-            shutil.copy(os.path.join(pasta_origem, produto + '.jpg'), pasta_destino)
+            output.put_markdown(f"- {produto} não encontrado")
+            tqdm.tqdm.write('Produto não encontrado: ' + produto)
 
+    # Cria o zip se existirem arquivos na pasta de destino
+    if os.listdir(pasta_destino):
+        make_archive('gerados/saida', 'zip', pasta_destino)
+
+        file = open('gerados/saida.zip', 'rb')
+        text = file.read()
+
+        output.put_file('saida.zip', text, 'Arquivo ZIP das Imagens')
     else:
-        tqdm.tqdm.write('Produto não encontrado: ' + produto)
+        output.put_markdown('#### *Não há arquivos para serem baixados* ')
+
+    # Chama o main novamente
+    main()
+
+
+# Inicia o servidor
+start_server(main, port=8082, debug=True)
